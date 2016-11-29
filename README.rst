cbd79c8c7c35e9839607dd179d1add87b6ee5841
rakhimov <ol.rakhimov@gmail.com>
2016-11-28 23:26:50 -0800

    Configure read_the_docs theme
    Issue #2

--- README.rst
+++ README.rst
@@ -3,3 +3,18 @@ The Open-PSA Model Exchange Format
 ##################################
 
 This repository hosts source text files for the Open-PSA Model Exchange Format.
+
+
+Building
+========
+
+A list of dependencies:
+
+====================   ===============
+Package                Minimum Version
+====================   ===============
+`Sphinx`               1.4.3
+`sphinx_rtd_theme`     0.1.8
+`LaTeX`
+`DVI-to-PNG`           1.14
+====================   ===============
--- conf.py
+++ conf.py
@@ -20,11 +20,13 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+import sphinx_rtd_theme
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
-# needs_sphinx = '1.0'
+needs_sphinx = '1.4'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
@@ -83,7 +85,7 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'README.rst']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
@@ -122,7 +124,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -132,6 +134,7 @@ html_theme = 'alabaster'
 
 # Add any paths that contain custom themes here, relative to this directory.
 # html_theme_path = []
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.
 # "<project> v<release> documentation" by default.
